fa093866668a16de2fb674e7688060aa2c720571
Paul <paul@gmail.com>
2020-04-24 18:06:26 +0200

    moved to ts  + vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/header/Header.vue";
+
+@Component({
+  components: {
+    HelloWorld,
+    Header
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,32 @@
+<style scoped lang="scss">
+.navbar-default {
+  background-color: #f8f8f8;
+  border-color: #e7e7e7;
+}
+.navbar-default .navbar-nav > li > a {
+  color: #777;
+}
+</style>
+
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <!--fixed="top"-->
+    <b-navbar-brand href="#">Lord of the rings</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item
+          v-for="(headerItem, index) in headerItems"
+          :key="index"
+          :href="headerItem.ref"
+          >{{ headerItem.name }}</b-nav-item
+        >
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script lang="ts" src="./Header.ts"></script>
